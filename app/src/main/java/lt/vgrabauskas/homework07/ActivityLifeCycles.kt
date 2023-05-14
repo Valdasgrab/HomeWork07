6a24b5d9400b6ea6af10f12aa7daa3a3417e7cd7
v008s <valdasgrab@gmail.com>
2023-05-14 17:18:55 +0300

    Add XML for note, activity_note, add NoteDetails class

---
+++ app/src/main/java/lt/vgrabauskas/homework07/ActivityLifeCycles.kt
@@ -0,0 +1,43 @@
+package lt.vgrabauskas.homework07
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+open class ActivityLifecycles : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+    }
+
+}
--- app/src/main/java/lt/vgrabauskas/homework07/CustomAdapter.kt
+++ app/src/main/java/lt/vgrabauskas/homework07/CustomAdapter.kt
@@ -5,12 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
 class CustomAdapter(context: Context) : BaseAdapter() {
 
     private val inflater = LayoutInflater.from(context)
     private val list = mutableListOf<Note>()
 
+    fun add (vararg note: Note) {
+        list.addAll(note)
+        notifyDataSetChanged()
+    }
+    fun add (notes: List<Note>) {
+        list.addAll(notes)
+        notifyDataSetChanged()
+    }
     override fun getCount(): Int =list.size
 
     override fun getItem(position: Int): Any = list[position]
@@ -18,7 +27,13 @@ class CustomAdapter(context: Context) : BaseAdapter() {
     override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val view = convertView ?: inflater.inflate(R.layout.note, parent, false)
+
+        view.findViewById<TextView>(R.id.idTextView).text = list[position].id.toString()
+        view.findViewById<TextView>(R.id.text01TextView).text = list[position].name
+        view.findViewById<TextView>(R.id.text02TextView).text = list[position].details
+        view.findViewById<TextView>(R.id.text03TextView).text = list[position].creationDate.toString()
+        return view
     }
 
 }
